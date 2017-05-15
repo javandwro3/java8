cb1985c290706f1193e0421e68094c912d4ecefa
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-05-15 20:31:51 +0200

    init

---
+++ src/main/java/pl/jwrabel/trainings/javandwro3/java8/MyConsumer.java
@@ -0,0 +1,8 @@
+package pl.jwrabel.trainings.javandwro3.java8;
+
+/**
+ * Created by jakubwrabel on 15/05/2017.
+ */
+public interface MyConsumer {
+	double multiply(double x);
+}
---
+++ src/main/java/pl/jwrabel/trainings/javandwro3/java8/TripleConsumer.java
@@ -0,0 +1,11 @@
+package pl.jwrabel.trainings.javandwro3.java8;
+
+/**
+ * Created by jakubwrabel on 15/05/2017.
+ */
+public class TripleConsumer implements MyConsumer {
+	@Override
+	public double multiply(double x) {
+		return x * 3;
+	}
+}
