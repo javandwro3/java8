3d41994e241bc7ea9d443b5205d2eb998addc7f4
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-05-16 19:31:44 +0200

    init

--- src/main/java/pl/jwrabel/trainings/javandwro3/java8/LambdaPersons.java
+++ src/main/java/pl/jwrabel/trainings/javandwro3/java8/LambdaPersons.java
@@ -18,5 +18,29 @@ public class LambdaPersons {
 
 
 		// WYPISANIE TYLKO OSÓB Z WARSZAWY
+		System.out.println("--- WYPISANIE OSÓB Z WARSZAWY ---");
+		personList.stream().filter(p -> p.getCity().equals("Warszawa")).forEach(x -> System.out.println(x));
+
+		// WYPISANIE TYLKO OSÓB Z WARSZAWY W FORMACIE (NAZWISKO Imię, Miasto)
+		// KOWALSKI Adam, Warszawa
+		System.out.println("--- WYPISANIE OSÓB Z WARSZAWY w podanym formacie ---");
+		personList.stream()
+				.filter(p -> p.getCity().equals("Warszawa"))
+				.map(p -> p.getLastName().toUpperCase() + " " + p.getFirstName() + ", " + p.getCity())
+				.forEach(x -> System.out.println(x));
+
+		// POSORTOWAC OSOBY PO NAZWISKACH
+		System.out.println("--- OSOBY POSORTOWANE PO NAZWISKACH ---");
+		personList.stream()
+				.sorted((p1, p2) -> p1.getLastName().compareTo(p2.getLastName()))
+//				.sorted((p1, p2) -> {
+//							return p1.getLastName().compareTo(p2.getLastName());
+//						}
+//				)
+				.forEach(x -> System.out.println(x));
+
+		// POSORTOWAC OSOBY PO Nazwisku, Imieniu, Mieście
+
+
 	}
 }
